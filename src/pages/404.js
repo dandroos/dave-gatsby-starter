55558828fde48ef2b383ce3d14372f495043892d
dandroos <dandrewsuk82@gmail.com>
2021-01-16 17:13:13 +0000

    Autogit: 16-01-2021 17:13

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,15 @@
 import React from "react"
-
-import Layout from "../components/layout"
+import { Typography } from "@material-ui/core"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <Typography variant="h2">404: Not Found</Typography>
+    <Typography>
+      You just hit a route that doesn&#39;t exist... the sadness.
+    </Typography>
+  </>
 )
 
 export default NotFoundPage
